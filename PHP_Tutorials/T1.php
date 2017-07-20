caea29de0bc44fa19854c40d456cf13aa32e273f
ViktorPavlicek <viktropavlicek@gmail.com>
2017-07-20 14:24:20 +0100

    T1 added
    Added T1 directory with index.php

--- PHP_Tutorials/T1.php
+++ PHP_Tutorials/T1.php
@@ -6,7 +6,6 @@
 <?php # nizovi
     $string = "Univerzitet Singidunum";
     
-    
     $niz = [1 ,2, 33, 44, 10, 45, -120, 2, 8];
     
     echo $string[1]; # 0 je prvi karakter u string-u! tj. (U-0,n-1,i-2...)
@@ -30,8 +29,32 @@ echo "<br>";
     echo $niz2[1]; # nako rabijanja na 2 elementa prikazacemo samo 2 element u nizu
     
 ?>    
+<br>   
+<?php
+    $naziv = " Univerzitet Singidunum ";
     
-
+    echo "Duzina stringa " . $naziv . " je: " . strlen($naziv) . "<br>"; # br. karaktera
+    
+    echo "Broj elemenata: " . count(explode(" ",$naziv)) . "<br>";
+    
+    echo "Prva 4 slova su: " . substr($naziv, 0, 4) . "<br>";
+    
+    echo "Slova 4, 5 i 6 su: " . substr($naziv, 3, 3) . "<br>"; 
+    
+    echo "Sve od 12 slova pa na dalje je: " . substr($naziv, 12) . "<br>";
+    
+    echo "Sve od 10 slova pa na dalje je: " . substr($naziv, 10) . "<br>"; # razmak nezavisno koliki je racuna se kao jedan karakter
+    
+    echo "Sve malim slovima: " . strtolower($naziv) . "<br>";
+    
+    echo "Sve velikim slovima: " . strtoupper($naziv) . "<br>";
+    
+    $vreme = "2016-02-06 11:14:51"; # Datum i vreme u ISO formatu
+    
+    $unizVreme = strtotime($vreme);
+    
+    echo $unizVreme . " je " . $vreme . "<br>"; # 1454753691 - br. sekundi koji je prosao od 1. januara 1970 u ponoc do ovog datuma koji smo ispisali  
+ ?>  
 
     
     
